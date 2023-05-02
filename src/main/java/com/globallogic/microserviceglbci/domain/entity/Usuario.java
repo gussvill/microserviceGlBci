0200a32dc027ad56170787db6a072217fb66bc0e
Gustavo Villegas <gussvill@gmail.com>
2023-05-02 00:28:57 -0400

    refactor code

--- src/main/java/com/globallogic/microserviceglbci/domain/entity/Usuario.java
+++ src/main/java/com/globallogic/microserviceglbci/domain/entity/Usuario.java
@@ -1,10 +1,5 @@
 package com.globallogic.microserviceglbci.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -12,11 +7,6 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "Usuario")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -49,4 +39,79 @@ public class Usuario {
         this.password = password;
     }
 
+    public Usuario() {
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(String lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
 }
